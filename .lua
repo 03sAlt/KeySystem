5d28e007bdc45512cc150c2fbe46704098bbc9ad
03sAlt <102461727+03sAlt@users.noreply.github.com>
2022-05-24 07:13:26 -1200

    Update and rename README.md to .lua

---
+++ .lua
@@ -0,0 +1,7 @@
+--- Discord invite
+loadstring(game:HttpGet("https://raw.githubusercontent.com/03koios/AutoinviteDiscord03s/main/README.md"))()
+
+--- Loader/GamePlace.id
+loadstring(game:HttpGet("https://raw.githubusercontent.com/03koios/KeySystem/main/.lua"))()
+
+--- discord.gg/x4gGhjVxXz
