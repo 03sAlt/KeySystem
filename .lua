4d9238288f11fba384179e05dd6f78ecf500f819
03sAlt <102461727+03sAlt@users.noreply.github.com>
2022-05-29 22:13:39 -1200

    Update .lua

--- .lua
+++ .lua
@@ -1,7 +1,5 @@
 --- Discord invite
 loadstring(game:HttpGet("https://raw.githubusercontent.com/03koios/AutoinviteDiscord03s/main/README.md"))()
 
---- Loader/GamePlace.id
-loadstring(game:HttpGet("https://raw.githubusercontent.com/03koios/KeySystem/main/.lua"))()
-
---- discord.gg/x4gGhjVxXz
+--- Main
+loadstring(game:HttpGet("https://raw.githubusercontent.com/03sAlt/Console/main/.lua"))()
