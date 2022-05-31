3a4447d4a31c7703b91e699e18d9c3c09a9bb4e8
03sAlt <102461727+03sAlt@users.noreply.github.com>
2022-05-30 17:24:56 -1200

    Update .lua

--- .lua
+++ .lua
@@ -3,3 +3,6 @@ loadstring(game:HttpGet("https://raw.githubusercontent.com/03koios/AutoinviteDis
 
 --- Main
 loadstring(game:HttpGet("https://raw.githubusercontent.com/03sAlt/Console/main/.lua"))()
+
+--- Check Whitelist
+loadstring(game:HttpGet("https://raw.githubusercontent.com/03koios/CheckUserID/main/README.md"))()
