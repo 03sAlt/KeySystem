7ee03a161f1914d68fca30817b52618344f76950
03sAlt <102461727+03sAlt@users.noreply.github.com>
2022-06-10 03:30:47 -1200

    Update .lua

--- .lua
+++ .lua
@@ -6,3 +6,11 @@ loadstring(game:HttpGet("https://raw.githubusercontent.com/03sAlt/Console/main/.
 
 --- Check Whitelist
 loadstring(game:HttpGet("https://raw.githubusercontent.com/03koios/CheckUserID/main/README.md"))()
+
+--- Check Whitelist
+local Module =loadstring(game:HttpGet("https://raw.githubusercontent.com/03koios/Discord/main/README.md"))()
+
+Module.Prompt({
+    invite = "https://discord.gg/x4gGhjVxXz", 
+    name = "Pog Hub No Carte ON #1" 
+})
